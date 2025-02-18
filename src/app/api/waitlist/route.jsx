3ba3be96cc6bbd9352017941bb1adaf8874160c3
Sushanth N <sushanthnandeti96@gmail.com>
2025-02-17 22:15:06 -0500

    API Fetch route added for waitlist

---
+++ src/app/api/waitlist/route.jsx
@@ -0,0 +1,35 @@
+'use server'
+
+import { NextResponse } from 'next/server';
+import {getToken} from '../../lib/auth';
+
+const DJANGO_API_WAITLIST_URL = "http://127.0.0.1:8001/api/waitlist/";
+
+
+export async function GET(request){
+
+    const authToken = await getToken()
+
+    if(!authToken) {
+        return NextResponse.json({},{status:401})
+    }
+    
+    const options = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${authToken}`,
+            'Accept' : "application/json"
+        },
+    }
+
+    const response = await fetch(DJANGO_API_WAITLIST_URL, options);
+    console.log(response)
+    let status = 200
+    if (!response.ok) {
+        status = 401
+    }
+
+     const result = await response.json();
+    return NextResponse.json({result}, { status: status})
+} 
