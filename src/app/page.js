4d72d377f226e7d22a84df1a128684803a345a47
Sushanth N <sushanthnandeti96@gmail.com>
2025-02-20 22:08:27 -0500

    Homeapage UI Change

--- src/app/page.js
+++ src/app/page.js
@@ -33,16 +33,7 @@ export default function Home() {
           height={38}
           priority
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
@@ -70,13 +61,7 @@ export default function Home() {
           </a>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+
           <Image
             aria-hidden
             src="/file.svg"
